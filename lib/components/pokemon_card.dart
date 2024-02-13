3f64e6c9a8205abe64482ec3984775bc293e3b10
Nabil Hardiansa <nhardiansa@gmail.com>
2024-02-14 06:57:09 +0700

    fix: make height and width of card being relative

--- lib/components/pokemon_card.dart
+++ lib/components/pokemon_card.dart
@@ -10,73 +10,75 @@ class PokemonCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Container(
-          // width: 104,
-          height: 108,
-          constraints: BoxConstraints(
-            maxWidth: MediaQuery.of(context).size.width / 4,
-            minWidth: MediaQuery.of(context).size.width / 4,
-          ),
-          decoration: BoxDecoration(
-              color: Colors.white,
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0x00000000).withOpacity(0.19),
+              offset: const Offset(0, 1),
+              blurRadius: 3,
+              spreadRadius: 1,
+            )
+          ]),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            height: 44,
+            decoration: BoxDecoration(
+              color: const Color(0xffefefef),
               borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0x00000000).withOpacity(0.19),
-                  offset: const Offset(0, 1),
-                  blurRadius: 3,
-                  spreadRadius: 1,
-                )
-              ]),
-        ),
-        Container(
-          width: 104,
-          height: 44,
-          decoration: BoxDecoration(
-            color: const Color(0xffefefef),
-            borderRadius: BorderRadius.circular(8),
+            ),
           ),
-        ),
-        Column(
-          children: [
-            Container(
-              // width: 104,
-              constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width / 4,
-                minWidth: MediaQuery.of(context).size.width / 4,
+          Column(
+            children: [
+              Container(
+                // width: 104,
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width / 1,
+                  minWidth: MediaQuery.of(context).size.width / 1,
+                ),
+                padding: const EdgeInsets.only(top: 4, right: 8),
+                child: Text(
+                  pokemonId,
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                      fontSize: 8,
+                      color: const Color(0xff666666),
+                      fontFamily: GoogleFonts.poppins().fontFamily),
+                ),
               ),
-              padding: const EdgeInsets.only(top: 16, right: 8),
-              child: Text(
-                pokemonId,
-                textAlign: TextAlign.end,
-                style: TextStyle(
-                    fontSize: 8,
-                    color: const Color(0xff666666),
-                    fontFamily: GoogleFonts.poppins().fontFamily),
+              Container(
+                constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.width / 4.9,
+                  minHeight: MediaQuery.of(context).size.width / 4.9,
+                ),
+                child: Image.network(
+                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+                ),
               ),
-            ),
-            Image.network(
-                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
-                width: 72,
-                height: 72),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 4, left: 8, right: 8),
-              child: Text(
-                pokemonName,
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                    height: 1.6,
-                    fontSize: 10,
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    fontFamily: GoogleFonts.poppins().fontFamily),
+              Container(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width / 1,
+                  minWidth: MediaQuery.of(context).size.width / 1,
+                ),
+                padding: const EdgeInsets.only(bottom: 4, left: 8, right: 8),
+                child: Text(
+                  pokemonName,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      height: 1.6,
+                      fontSize: 10,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontFamily: GoogleFonts.poppins().fontFamily),
+                ),
               ),
-            ),
-          ],
-        )
-      ],
+            ],
+          )
+        ],
+      ),
     );
   }
 }
